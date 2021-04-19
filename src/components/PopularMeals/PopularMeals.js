c3ec9887cca6b8b0e496d4ff22b1c5161bb9e4f3
Thomas Rago <thomasrago@Thomass-iMac.fios-router.home>
2021-04-19 17:01:48 -0400

    remove index

---
+++ src/components/PopularMeals/PopularMeals.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PopularMeals.scss";
+
+const PopularMeals = () => {
+  return (
+    <div className="PopularMeals">
+      <div className="pop-meals-container"></div>
+    </div>
+  );
+};
+
+export default PopularMeals;
